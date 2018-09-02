9630a06ae09f217bc07aa127cdd4e8de0ba8f781
praburangki <praburangki@gmail.com>
2018-09-02 16:11:29 +0800

    fix cors

--- config/application.rb
+++ config/application.rb
@@ -30,14 +30,6 @@ module Backend
       g.template_engine false
     end
 
-    config.middleware.insert_before 0, 'Rack::Cors' do
-      allow do
-        # For production, you need to replace the
-        # asterisk with the URL of your client-side application
-        origins '*'
-        resource '*', headers: :any, methods: %i[get post options]
-      end
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
